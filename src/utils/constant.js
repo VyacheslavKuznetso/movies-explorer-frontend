7e0de924b9fdbce701e6f878863cdd666421298d
Vyacheslav Kuznetsov <kuz.poch@yandex.ru>
2024-03-17 22:19:32 +0300

    исправил  Вывод карточек на экран при Нажатии на кнопку «Ещё», 17.03.24

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,13 +5,13 @@ export const DESKTOP_SETTINGS = {
 };
 
 export const TABLET_SETTINGS = {
-  maxWidth: 768,
+  maxWidth: 1279,
   defaultMovies: 8,
   moreStep: 2,
 };
 
 export const MOBILE_SETTINGS = {
-  maxWidth: 480,
+  maxWidth: 768,
   defaultMovies: 5,
   moreStep: 2,
 };
